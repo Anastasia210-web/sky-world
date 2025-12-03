266e116cd49423776e613f50633dfebfc6388f0d
Anastasia Arefeva <89157321250@mail.ru>
2025-12-03 18:32:39 +0300

    Сделала колонки "В работе" и "тестирование"

--- src/components/TaskBoard.vue
+++ src/components/TaskBoard.vue
@@ -2,12 +2,16 @@
     <div>
 <Column title="Без статуса"/>
 <ColumnAsk title="Нужно сделать"/>
+<ColumnWork title="В работе"/>
+<ColumnTest title="Тестирование"/>
     </div>
 </template>
 
 <script setup>
 import Column from './Column.vue';
 import ColumnAsk from './ColumnAsk.vue';
+import ColumnTest from './ColumnTest.vue';
+import ColumnWork from './ColumnWork.vue';
 
 
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,10 @@
   <div>
     <HomeHeader />
     <TaskBoard />
-    <ColumnAsk />
   </div>
 </template>
 
 <script setup>
-import ColumnAsk from "@/components/ColumnAsk.vue";
 import HomeHeader from "@/components/HomeHeader.vue";
 import TaskBoard from "@/components/TaskBoard.vue";
 </script>
